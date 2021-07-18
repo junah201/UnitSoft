730d73b2811d1ab0459d4698e08d0c3e78071494
white201 <white_201@naver.com>
2021-07-18 20:12:18 +0900

    [체험학습] 출력문 1

---
+++ 2423.c
@@ -0,0 +1,7 @@
+# include <stdio.h>                         
+int main()                                   
+{
+    printf("Hello");                     
+    printf("this is C program");       
+    return 0;                      
+}
