ebb9e986f72b4fe6c6c25c2c0bbb09591fa5bdfc
white201 <white_201@naver.com>
2021-07-18 20:35:42 +0900

    [체험학습] 출력문 4

---
+++ 2426.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("A     A\n");
+    printf(" B   B \n");
+    printf("  C C  \n");
+    printf("   D   \n");
+    printf("  E E  \n");
+    printf(" F   F \n");
+    printf("G     G\n");
+
+    return 0;
+}
