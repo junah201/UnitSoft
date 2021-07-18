33567867280ba22b96e81de0ef06a0638de5cb14
white201 <white_201@naver.com>
2021-07-18 21:17:52 +0900

    2차원 배열 출력 C

---
+++ 1097.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int Arr[100][100] = {0,};
+
+    int N;
+    scanf("%d",&N);
+
+    int Num=1;
+
+    for(int y = 0;y<N;y++){
+        for(int x=N-1;x>=0;x--){
+            Arr[x][y] = Num++;
+        }
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j =0;j<N;j++){
+            printf("%d ",Arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
