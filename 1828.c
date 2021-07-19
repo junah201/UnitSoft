33dde9072c244572c29b82719ef9d2e7cd7ab68c
white201 <white_201@naver.com>
2021-07-19 15:18:20 +0900

    [basic] 배열 연습문제-6

---
+++ 1828.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int arr[100];
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
+    }
+    int temp;
+    for (int i=0;i<N;i++){
+        for (int j = 0; j < N - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("%d %d",arr[N-1],arr[N-2]);
+    return 0;
+}
