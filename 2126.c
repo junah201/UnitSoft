6a3afc6ba26a16f005dda639e1aead2f006737cb
white201 <white_201@naver.com>
2021-07-20 14:45:57 +0900

    [basic] 이차원배열 연습문제-11

---
+++ 2126.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int arr[100][100]={0,};
+
+    int N,M;
+    scanf("%d %d",&N,&M);
+    int num=1;
+    for(int i=M-1;i>=0;i--){
+        for(int j=0;j<N;j++){
+            arr[j][i]=num++;
+        }
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            printf("%5d",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
