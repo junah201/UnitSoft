ae598b70fc400f232437007e961be5c600247cf6
white201 <white_201@naver.com>
2021-07-20 15:17:18 +0900

    [basic] 이차원배열 연습문제-15

---
+++ LV 4. 배열/2130.c
@@ -0,0 +1,29 @@
+//2130. [basic] 이차원배열 연습문제-15
+
+#include <stdio.h>
+
+int main(){
+    int arr[100][100]={0,};
+
+    int N;
+    scanf("%d",&N);
+    int num=1;
+    int y=N-1;
+    int add=-1;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            arr[y][i]=num++;
+            y+=add;
+        }
+        y-=add;
+        add*=-1;
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            printf("%5d",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
