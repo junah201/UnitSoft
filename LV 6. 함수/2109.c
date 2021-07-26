a74cfe217a3479be1084d6e16890332926cef97c
white201 <white_201@naver.com>
2021-07-26 16:16:29 +0900

    [basic] 기초 함수 실습 문제 -9

---
+++ LV 6. 함수/2109.c
@@ -0,0 +1,16 @@
+//2109. [basic] 기초 함수 실습 문제 -9
+
+#include <stdio.h>
+
+void count(){
+	printf("count()\n");
+	for(int i=1;i<=10;i++){
+		printf("%d\n",i);
+	}
+}
+
+int main()
+{
+	count();
+	return 0;
+}
