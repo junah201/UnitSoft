3695c1ceb1bb85a3c15cd85c93348581786eafd1
white201 <white_201@naver.com>
2021-08-01 16:33:11 +0900

    [basic] 재귀함수 실습문제 11 (3,6,9,3,6,9 ver3)

---
+++ LV 7. 재귀 함수/2167.c
@@ -0,0 +1,22 @@
+//2167. [basic] 재귀함수 실습문제 11 (3,6,9,3,6,9 ver3)
+
+#include <stdio.h>
+
+int count(int n,int end){
+    if(n > end){
+        return 0;
+    }
+    if(n-n/10*10==3 || n%3==0){
+        printf("  X");
+    }else{
+        printf("%3d",n);
+    }
+    count(++n,end);
+}
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    count(1,N);
+    return 0;
+}
