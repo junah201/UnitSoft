de3647c94faefbcdef57275eeac61cdc48288e1c
white201 <white_201@naver.com>
2021-08-01 16:42:06 +0900

    2217.c

---
+++ LV 7. 재귀 함수/2217.c
@@ -0,0 +1,26 @@
+//2217. [basic] 재귀함수 실습문제 23
+
+#include <stdio.h>
+
+int num = 0;
+int add = 1;
+
+int count(int n){
+    printf("%d\n",n);
+    if(n==1){
+        return 0;
+    }
+    if(n%2==0){
+        n=n/2;
+    }else{
+        n=3*n+1;
+    }
+    count(n);
+}
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    count(N);
+    return 0;
+}
