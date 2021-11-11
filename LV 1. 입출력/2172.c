3df66e077cb2678fa308e24178056b5ee1c330de
junah201 <junah.dev@gmail.com>
2021-11-11 19:59:23 +0900

    [basic] 입출력 연습문제 12

---
+++ LV 1. 입출력/2172.c
@@ -0,0 +1,14 @@
+//2172. [basic] 입출력 연습문제 12
+
+# include <stdio.h>
+
+int main() {
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d - %d = %d\n",a,b,a-b);
+	printf("%d * %d = %d\n",a,b,a*b);
+	printf("%d / %d = %d\n",a,b,a/b);
+	printf("%d %% %d = %d\n",a,b,a%b);
+	return 0;
+}
