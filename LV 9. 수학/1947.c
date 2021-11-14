057ec0d20581959636e085fbd8378bf2af87bf97
junah201 <junah.dev@gmail.com>
2021-11-15 00:22:51 +0900

    유클리드 호제법 (재귀)

---
+++ LV 9. 수학/1947.c
@@ -0,0 +1,17 @@
+//1947. 유클리드 호제법 (재귀)
+
+#include <stdio.h>
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+
+int get(int a, int b) {
+    if(a%b==0) return b;
+    else return get(b, a%b);
+}
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",get(max(a,b),min(a,b)));
+    return 0;
+}
