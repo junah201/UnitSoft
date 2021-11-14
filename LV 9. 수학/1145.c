295f7e8cdc9c46d1f00ffd7f75f4e4a459923bdc
junah201 <junah.dev@gmail.com>
2021-11-15 00:18:57 +0900

    약수의 합 구하기1

--- LV 9. 수학/1145.c
+++ LV 9. 수학/1145.c
@@ -1,1 +1,15 @@
 //1145. 약수의 합 구하기1
+
+#include <stdio.h>
+
+int main() {
+    int a,sum=0;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++) {
+        if(a%i==0) {
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
