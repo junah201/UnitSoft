7e205ca1d3c026aba8efb6bbc1fbc954a231aa07
junah201 <junah.dev@gmail.com>
2021-11-19 23:01:53 +0900

    [basic] 입출력 연습문제 13

---
+++ LV 1. 입출력/2173.c
@@ -0,0 +1,10 @@
+//2173. [basic] 입출력 연습문제 13
+
+#include <stdio.h>
+
+int main() {
+    double a1,a2,b1,b2,c1,c2;
+    scanf("%lf %lf %lf %lf %lf %lf",&a1,&a2,&b1,&b2,&c1,&c2);
+    printf("%.1lf",(a1*a2+b1*b2+c1*c2));
+    return 0;
+}
