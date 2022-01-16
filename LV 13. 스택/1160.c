f1db0d27ed3f9266a508d17d7676fa9ae86729fd
junah201 <junah.dev@gmail.com>
2022-01-16 10:53:41 +0900

    Catalan Number

--- LV 13. 스택/1160.c
+++ LV 13. 스택/1160.c
@@ -5,46 +5,27 @@
 
 using namespace std;
 
-int n,map[100];
-stack<int> s;
-int start;
-int e;
+int N,idx,value,map[100000];
 
-int check() {
-    for(int i=1;i<=n;i++) {
-        if(s.top()!=i) return 0;
-        s.pop();
-    }
-    return 1;
-}
+stack<int> s;
 
 int main() {
-    scanf("%d",&n);
-    for(int i=0;i<n;i++) {
-        scanf("%d",&map[i]);
-    }
-    start=n-1;
-    e=n-1;
-    while(1) {
-        //printf("s:%d,e:%d\n",start,e);
-        if(start<0) {
-            break;
+    scanf("%d", &N);
+    for(int i=0;i<N;i++) scanf("%d",&map[i]);
+    value = 1;
+    for(int i=0;i<N;i++) {
+        while(s.empty() || s.top() <map[i]) {
+            s.push(value++);
         }
-        if(map[start-1]-1!=map[e]) {
-            for(int i=start;i<=e;i++) {
-                s.push(map[i]);
-            }
-            e=start-1;
-            start=start-1;
-            continue;
+        if(s.top() == map[i]) {
+            s.pop();
+            idx++;
         }
-        start--;
+        else {
+            printf("NO\n");
+            return 0;
+        }
     }
-    if(check()) {
-        printf("YES\n");
-    }
-    else {
-        printf("NO\n");
-    }
+    printf("YES\n");
     return 0;
 }
