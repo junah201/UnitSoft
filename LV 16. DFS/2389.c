78c0773b0486256f69e9d23c74426fa89cabdea4
junah201 <junah.dev@gmail.com>
2022-01-29 02:05:12 +0900

    마이야르 중심방송 류빈아빠

--- LV 16. DFS/2389.c
+++ LV 16. DFS/2389.c
@@ -42,7 +42,73 @@ int cnt_bfs(int x, int y) {
 	return 0;
 }
 
-int set_bfs(int x, int y) {
+int set_bfs(int x, int y) {//2389. 마이야르 중심방송 류빈아빠
+
+#include <stdio.h>
+
+int map[1001][1001];
+int ids[100000];
+int used[4];
+int check[1001][1001];
+
+int N, M;
+int max, cnt, num;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+int check_id(int id) {
+	for(int i=0;i<4;i++) if(used[i] == id) return 0;
+	return 1;
+}
+int dfs(int x, int y) {
+	cnt++;
+	map[y][x] = num;
+	for(int i=0;i<4;i++) {
+		if(map[y+dy[i]][x+dx[i]] == 1 && check[y+dy[i]][x+dx[i]] == 0) {
+			check[y+dy[i]][x+dx[i]] = 1;
+			dfs(x+dx[i], y+dy[i]);
+		}
+	}
+	return 0;
+}
+int main() {
+	scanf("%d %d", &N, &M);
+	for(int y=1;y<=N;y++) {
+		for(int x=1;x<=M;x++) {
+			scanf("%d", &map[y][x]);
+		}
+	}
+	for(int y=1;y<=N;y++) {
+		for(int x=1;x<=M;x++) {
+			if(map[y][x]==1 && check[y][x]==0) {
+				num++;
+				cnt = 0;
+				check[y][x] = 1;
+				dfs(x, y);
+				ids[num] = cnt;
+			}
+		}
+	}
+	for(int y=1;y<=N;y++) {
+		for(int x=1;x<=M;x++) {
+			if(map[y][x]==0) {
+				int result = 0;
+				for (int i=0;i<4;i++) used[i] = 0;
+				for(int i=0;i<4;i++) {
+					if(check_id(map[y+dy[i]][x+dx[i]])==1) {
+						used[i] = map[y+dy[i]][x+dx[i]];
+						result += ids[map[y+dy[i]][x+dx[i]]];
+					}
+				}
+				if(result > max) max = result;
+			}
+		}
+	}
+	printf("%d", max+1);
+	return 0;
+}
+
 	map[y][x] = cnt;
 	id[y][x] = num;
 	for(int i=0;i<4;i++) {
