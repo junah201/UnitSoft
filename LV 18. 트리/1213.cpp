9ed33e16657e9038efc01a249c3ace979c3fba0a
junah201 <junah.dev@gmail.com>
2022-02-23 16:15:48 +0900

    subsequence

---
+++ LV 18. 트리/1213.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int N, S;
+int map[100002];
+
+int bs()
+{
+    int ans = 987654321;
+    int low = 0;
+    int high = 1;
+    int sum = map[0];
+    while (high <= N)
+    {
+        if (sum < S)
+        {
+            sum += map[high++];
+        }
+        else if (sum >= S)
+        {
+            if (ans > high - low)
+                ans = high - low;
+            sum -= map[low++];
+        }
+    }
+    if (ans == 987654321)
+        return 0;
+    return ans;
+}
+
+int main()
+{
+    scanf("%d %d", &N, &S);
+    for (int i = 0; i < N; i++)
+        scanf("%d", &map[i]);
+    printf("%d", bs());
+    return 0;
+}
