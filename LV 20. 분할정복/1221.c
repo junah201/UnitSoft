c90820d7b9b0f671fe647a6ce1fec5351ef55827
junah201 <junah.dev@gmail.com>
2022-02-27 17:43:40 +0900

    세모별찍기

---
+++ LV 20. 분할정복/1221.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int N;
+int map[1024 * 3][1024 * 6];
+
+void get(int idx, int x, int y) {
+    if(idx == 3) {
+        map[y][x] = 1;
+        map[y+1][x-1] = 1;
+        map[y+1][x+1] = 1;
+        map[y+2][x-2] = 1;
+        map[y+2][x-1] = 1;
+        map[y+2][x] = 1;
+        map[y+2][x+1] = 1;
+        map[y+2][x+2] = 1;
+        return;
+    }
+    get(idx / 2, x - idx/2, y + idx/2);
+    get(idx / 2, x, y);
+    get(idx / 2, x + idx/2, y + idx/2);
+}
+
+int main() {
+    scanf("%d", &N);
+    get(N, N-1, 0);
+    for(int y=0;y<=N-1;y++) {
+        for(int x=0;x<=N*2-1;x++) {
+            if(map[y][x] == 0) printf(" ");
+            else printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
