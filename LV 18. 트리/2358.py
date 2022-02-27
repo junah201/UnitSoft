e24cfee5bd62e6c297096dda7c477b5263125e14
junah201 <junah.dev@gmail.com>
2022-02-27 17:39:54 +0900

    닉네임의 닉네임

--- LV 18. 트리/2358.py
+++ LV 18. 트리/2358.py
@@ -1,11 +1,20 @@
+# 2358. 닉네임의 닉네임
+
+prefix = {}
+
 N = int(input())
-nick = {}
 for _ in range(N):
-    user = input()
-    temp = ""
-    for i in list(user):
-        temp += i
-        if nick.get(temp) == None:
-            print(temp)
-            nick[temp] = user
-            break
+    nick = input()
+    check = True
+    for i in range(1,len(nick)+1):
+        # print(nick[:i])
+        if prefix.get(nick[:i]) == None and check:
+            print(nick[:i])
+            check = False
+        prefix[nick[:i]] = True
+    if check:
+        for i in range(2, 100002):
+            if prefix.get(nick + str(i)) == None:
+                prefix[nick + str(i)] = True
+                print(nick + str(i))
+                break;
